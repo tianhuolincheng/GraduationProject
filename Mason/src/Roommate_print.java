56e6bc7f00467db9823aaa883151b745f18d14bf
tianhuolincheng <98543903+tianhuolincheng@users.noreply.github.com>
2022-11-10 23:08:21 +0800

    Push java Project

---
+++ Mason/src/Roommate_print.java
@@ -0,0 +1,262 @@
+import sim.engine.SimState;
+import sim.engine.Steppable;
+import sim.field.continuous.Continuous2D;
+import sim.field.network.Edge;
+import sim.util.Bag;
+import sim.util.Double2D;
+import sim.util.MutableDouble2D;
+
+public class Roommate_print implements Steppable
+{
+    public int dormNum = -1;
+    public Bag myDorm = new Bag();
+
+    public double[][][] action_value = new double[5][4][4]; // 5 time points; 4 values; 4 alternative actions.
+    // initialize the action_value array:
+
+    public double[][] value_weights = new double[5][4]; // 5 time points; 4 values.
+
+    public int norm_TimePoint = -1;
+
+    public int norm_Activity = -1;
+
+    public double[] utility = new double[2000];
+    public double[] utility_primary = new double[2000];
+    public double[] utility_conservative = new double[2000];
+    public int current_num = 0;
+
+    public void step(SimState state) {
+        Roommates_print roommates_print = (Roommates_print) state;
+        double rand_num = roommates_print.random.nextDouble();
+        int time_point = -1;
+
+        // Define Threshold;
+        double threshold_1_outof_4 = 0.75;
+        double threshold_2_outof_4 = 0.5;
+        double threshold_3_outof_4 = 0.25;
+
+        double threshold_1_outof_5 = 0.8;
+        double threshold_2_outof_5 = 0.6;
+        double threshold_3_outof_5 = 0.4;
+        double threshold_4_outof_5 = 0.2;
+
+        // Random select a time point
+        if (rand_num > threshold_1_outof_5){
+            time_point = 0;
+        } else if (rand_num > threshold_2_outof_5) {
+            time_point = 1;
+        } else if (rand_num > threshold_3_outof_5) {
+            time_point = 2;
+        } else if (rand_num > threshold_4_outof_5){
+            time_point = 3;
+        } else {
+            time_point = 4;
+        }
+
+        // Find this agent's roommate
+        Bag dorm = this.myDorm;
+        Roommate_print agent_roommate = (Roommate_print) dorm.get(0);
+        roommates_print.temp_num_for_output++;
+
+        // For each value, compute the maximum and minimum utility.
+        // (If the norm of this agent or its roommate is conflicted, the utility will be subtracted by 0.5.)
+        double[] me_max = new double[4];
+        double[] me_min = new double[4];
+        double[] other_max = new double[4];
+        double[] other_min = new double[4];
+
+        // Record the S, R, and Q values for decision-making:
+        double[] S_value = new double[4];
+        double[] R_value = new double[4];
+        double[] Q_value = new double[4];
+
+        // For each value, find its maximum and minimum utility for this agent and its roommate
+        for (int v=0; v<4; v++){
+            // Initialize the maximum and minimum utility for value v:
+            me_max[v] = 0;
+            me_min[v] = 0.5;
+            other_max[v] = 0;
+            other_min[v] = 0.5;
+
+            // For each alternative activity, update the maximum and minimum utility for value v:
+            for (int a=0; a<4; a++){
+                double adjust = 0; // Adjust for the activity that is prohibited by norms
+                if ((this.norm_TimePoint == time_point && this.norm_Activity == a) ||
+                        (agent_roommate.norm_TimePoint == time_point && agent_roommate.norm_Activity == a)){
+                    adjust = -0.5;
+                }
+                if (this.action_value[time_point][v][a] + adjust > me_max[v]){
+                    me_max[v] = this.action_value[time_point][v][a] + adjust;
+                }
+                if (agent_roommate.action_value[time_point][v][a] + adjust > other_max[v]){
+                    other_max[v] = agent_roommate.action_value[time_point][v][a] + adjust;
+                }
+                if (this.action_value[time_point][v][a] + adjust < me_min[v]){
+                    me_min[v] = this.action_value[time_point][v][a] + adjust;
+                }
+                if (agent_roommate.action_value[time_point][v][a] + adjust < other_min[v]){
+                    other_min[v] = agent_roommate.action_value[time_point][v][a] + adjust;
+                }
+            }
+        }
+
+        // Maximum and minimum value of S and R. They are initialized to be the first activity.
+        int S_max = 0;
+        int S_min = 0;
+        int R_max = 0;
+        int R_min = 0;
+
+        for (int a=0; a<4; a++){
+            // Initialize the S,R and Q values:
+            S_value[a] = 0;
+            R_value[a] = 0;
+            Q_value[a] = 0;
+
+            double adjust = 0; // Adjust for the activity that is prohibited by norms
+            if ((this.norm_TimePoint == time_point && this.norm_Activity == a) ||
+                    (agent_roommate.norm_TimePoint == time_point && agent_roommate.norm_Activity == a)){
+                adjust = -0.5;
+            }
+
+            double temp_weighted; // A temporary number for convenience: it represents the weighted utility;
+
+            // For each activity, compute the weighted and normalized Manhattan distance and Chebyshev distance
+            for (int v=0; v<4; v++){
+                if (me_max[v] - me_min[v] != 0){
+                    temp_weighted = this.value_weights[time_point][v] * (me_max[v] - this.action_value[time_point][v][a] - adjust)/(me_max[v] - me_min[v]);
+                    S_value[a] += temp_weighted;
+                    if (temp_weighted > R_value[a]){
+                        R_value[a] = temp_weighted;
+                    }
+                }
+                if (other_max[v] - other_min[v] != 0){
+                    temp_weighted = agent_roommate.value_weights[time_point][v] * (other_max[v] - agent_roommate.action_value[time_point][v][a] - adjust)/
+                            (other_max[v] - other_min[v]);
+                    S_value[a] += temp_weighted;
+                    if (temp_weighted > R_value[a]){
+                        R_value[a] = temp_weighted;
+                    }
+                }
+            }
+
+            // Update the maximum and minimum values of S and R:
+            if (a != 0){
+                if (S_value[a] > S_value[S_max]){
+                    S_max = a;
+                }
+                if (S_value[a] < S_value[S_min]){
+                    S_min = a;
+                }
+                if (R_value[a] > R_value[R_max]){
+                    R_max = a;
+                }
+                if (R_value[a] < R_value[R_min]){
+                    R_min = a;
+                }
+            }
+        }
+
+        // Record the activity that minimizes the Q value:
+        int Q_min = 0;
+
+        // For each activity, compute the Q which combines the Manhattan distance and Chebyshev distance
+        for (int a=0; a<4; a++){
+            if (S_value[S_max] - S_value[S_min] != 0){
+                Q_value[a] += 0.5 * (S_value[a] - S_value[S_min])/(S_value[S_max] - S_value[S_min]);
+            }
+            if (R_value[R_max] - R_value[R_min] != 0){
+                Q_value[a] += 0.5 * (R_value[a] - R_value[S_min])/(R_value[R_max] - R_value[R_min]);
+            }
+            if (Q_value[a] < Q_value[Q_min]){
+                Q_min = a;
+            }
+        }
+
+        // Record the activity that maximizes the primary agent's utility
+        int a_primary = 0;
+        double max_value_sum = 0;
+
+        for (int a=0; a<4; a++){
+            double value_sum = 0;
+            double adjust = 0.0; // Adjust for the activity that is prohibited by norms
+            if ((this.norm_TimePoint == time_point && this.norm_Activity == a) ||
+                    (agent_roommate.norm_TimePoint == time_point && agent_roommate.norm_Activity == a)){
+                adjust = -0.5;
+            }
+            for (int v=0; v<4; v++){
+                value_sum = value_sum + (this.action_value[time_point][v][a] + adjust) * this.value_weights[time_point][v];
+            }
+            if (roommates_print.distri_num < 10000){
+                roommates_print.utility_distri[roommates_print.distri_num] = value_sum;
+                roommates_print.distri_num ++;
+            }
+            if (value_sum > max_value_sum){
+                max_value_sum = value_sum;
+                a_primary = a;
+            }
+        }
+
+        // Record the activity for the conservative decision-making strategy.
+        int a_conservative = 0;
+        if (time_point < 4){
+            a_conservative = time_point;
+        }
+
+        // Record the utility for the agent and its roommate:
+        double adjust = 0.0; // Adjust for the activity that is prohibited by norms
+        if ((this.norm_TimePoint == time_point && this.norm_Activity == Q_min) ||
+                (agent_roommate.norm_TimePoint == time_point && agent_roommate.norm_Activity == Q_min)){
+            adjust = -0.5;
+        }
+        double adjust_primary = 0.0; // Adjust for the activity that is prohibited by norms
+        if ((this.norm_TimePoint == time_point && this.norm_Activity == a_primary) ||
+                (agent_roommate.norm_TimePoint == time_point && agent_roommate.norm_Activity == a_primary)){
+            adjust_primary = -0.5;
+        }
+        double adjust_conservative = 0.0; // Adjust for the activity that is prohibited by norms
+        if ((this.norm_TimePoint == time_point && this.norm_Activity == a_conservative) ||
+                (agent_roommate.norm_TimePoint == time_point && agent_roommate.norm_Activity == a_conservative)){
+            adjust_conservative = -0.5;
+        }
+        for (int v=0; v<4; v++){
+            this.utility[this.current_num] += this.value_weights[time_point][v] * (this.action_value[time_point][v][Q_min] + adjust);
+            agent_roommate.utility[agent_roommate.current_num] += agent_roommate.value_weights[time_point][v] *
+                    (agent_roommate.action_value[time_point][v][Q_min] + adjust);
+            this.utility_primary[this.current_num] += this.value_weights[time_point][v] * (this.action_value[time_point][v][a_primary] + adjust_primary);
+            agent_roommate.utility_primary[agent_roommate.current_num] += agent_roommate.value_weights[time_point][v] *
+                    (agent_roommate.action_value[time_point][v][a_primary] + adjust_primary);
+            this.utility_conservative[this.current_num] += this.value_weights[time_point][v] * (this.action_value[time_point][v][a_conservative] + adjust_conservative);
+            agent_roommate.utility_conservative[agent_roommate.current_num] += agent_roommate.value_weights[time_point][v] *
+                    (agent_roommate.action_value[time_point][v][a_conservative] + adjust_conservative);
+        }
+        this.current_num ++;
+        agent_roommate.current_num ++;
+
+        // Change the norm for the agent
+        // 1. Randomly select a time point for this agent's norm:
+        double rand_num_norm = roommates_print.random.nextDouble();
+        if (rand_num_norm > threshold_1_outof_5){
+            this.norm_TimePoint = 0;
+        } else if (rand_num_norm > threshold_2_outof_5) {
+            this.norm_TimePoint = 1;
+        } else if (rand_num_norm > threshold_3_outof_5) {
+            this.norm_TimePoint = 2;
+        } else if (rand_num_norm > threshold_4_outof_5) {
+            this.norm_TimePoint = 3;
+        } else {
+            this.norm_TimePoint = 4;
+        }
+
+        // 2. Randomly select an activity for this agent's norm:
+        rand_num_norm = roommates_print.random.nextDouble();
+        if (rand_num_norm > threshold_1_outof_4){
+            this.norm_Activity = 0;
+        } else if (rand_num_norm > threshold_2_outof_4) {
+            this.norm_Activity = 1;
+        } else if (rand_num_norm > threshold_3_outof_4) {
+            this.norm_Activity = 2;
+        } else {
+            this.norm_Activity = 3;
+        }
+    }
+}
---
+++ Mason/src/Roommates_print.java
@@ -0,0 +1,248 @@
+import sim.engine.SimState;
+import sim.engine.Steppable;
+import sim.field.continuous.Continuous2D;
+import sim.field.network.Network;
+import sim.util.Bag;
+import sim.util.Double2D;
+import sim.util.Interval;
+import java.io.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Roommates_print extends SimState
+{
+    public int numRoommates = 1000;
+    public Bag allRoommates = new Bag();
+    public Roommates_print(long seed)
+    {
+        super(seed);
+    }
+    public int temp_num_for_output = 0;
+    public double[][] utility_record = new double[numRoommates][2000];
+    public double[][] utility_record_primary = new double[numRoommates][2000];
+    public double[][] utility_record_conservative = new double[numRoommates][2000];
+
+    public double[] utility_distri = new double[10000];
+    public int distri_num = 0;
+
+    public void start(){
+        super.start();
+
+        // Create new roommates
+        for(int i = 0; i < numRoommates; i++) {
+            Roommate_print roommate_print = new Roommate_print();
+
+            // Define Dorms;
+            roommate_print.dormNum = (int)(i/2);
+            allRoommates.add(roommate_print);
+
+            // Define Threshold;
+            double threshold_1_outof_4 = 0.75;
+            double threshold_2_outof_4 = 0.5;
+            double threshold_3_outof_4 = 0.25;
+
+            double threshold_1_outof_5 = 0.8;
+            double threshold_2_outof_5 = 0.6;
+            double threshold_3_outof_5 = 0.4;
+            double threshold_4_outof_5 = 0.2;
+
+            double threshold_1_outof_6 = 5.0/6.0;
+            double threshold_2_outof_6 = 4.0/6.0;
+            double threshold_3_outof_6 = 3.0/6.0;
+            double threshold_4_outof_6 = 2.0/6.0;
+            double threshold_5_outof_6 = 1.0/6.0;
+
+            // Initialize each agent's norm:
+            // Each agent prohibits a certain activity at a certain time point.
+            // 1. Randomly select a time point for this agent's norm:
+            double rand_num_norm = random.nextDouble();
+            if (rand_num_norm > threshold_1_outof_5){
+                roommate_print.norm_TimePoint = 0;
+            } else if (rand_num_norm > threshold_2_outof_5) {
+                roommate_print.norm_TimePoint = 1;
+            } else if (rand_num_norm > threshold_3_outof_5) {
+                roommate_print.norm_TimePoint = 2;
+            } else if (rand_num_norm > threshold_4_outof_5) {
+                roommate_print.norm_TimePoint = 3;
+            } else {
+                roommate_print.norm_TimePoint = 4;
+            }
+
+            // 2. Randomly select an activity for this agent's norm:
+            rand_num_norm = random.nextDouble();
+            if (rand_num_norm > threshold_1_outof_4){
+                roommate_print.norm_Activity = 0;
+            } else if (rand_num_norm > threshold_1_outof_4) {
+                roommate_print.norm_Activity = 1;
+            } else if (rand_num_norm > threshold_1_outof_4) {
+                roommate_print.norm_Activity = 2;
+            } else {
+                roommate_print.norm_Activity = 3;
+            }
+
+            // Initialize the action-value table for this roommate:
+            for (int m=0; m<5; m++){
+                for (int n=0; n<4; n++){
+                    for (int l=0; l<4; l++){
+                        roommate_print.action_value[m][n][l] = 0.0;
+                    }
+                }
+            }
+
+            // Initialize the value_weight array for this roommate:
+            for (int m=0; m<5; m++){
+                for (int n=0; n<4; n++){
+                    roommate_print.value_weights[m][n] = 1;
+                }
+            }
+
+            // For each time point (5 possible time points in total)
+            for (int t=0; t<5; t++){
+                // For each value (4 possible values in total)
+                // Set the value_weights array.
+                double rand_num = random.nextDouble();
+                if (rand_num > 0.5){ // With probability 0.5, the agent have one favorite value at a certain time point.
+                    double rand_num_ = random.nextDouble();
+                    if (rand_num_ > threshold_1_outof_4){
+                        roommate_print.value_weights[t][0] = 4;
+                    } else if (rand_num_ > threshold_2_outof_4) {
+                        roommate_print.value_weights[t][1] = 4;
+                    } else if (rand_num_ > threshold_3_outof_4) {
+                        roommate_print.value_weights[t][2] = 4;
+                    } else {
+                        roommate_print.value_weights[t][3] = 4;
+                    }
+                } else { // With probability 0.5, the agent have two equally favorite value at a certain time point.
+                    double rand_num_ = random.nextDouble();
+                    if (rand_num_ > threshold_1_outof_6){
+                        roommate_print.value_weights[t][0] = 2;
+                        roommate_print.value_weights[t][1] = 2;
+                    } else if (rand_num_ > threshold_2_outof_6) {
+                        roommate_print.value_weights[t][0] = 2;
+                        roommate_print.value_weights[t][2] = 2;
+                    } else if (rand_num_ > threshold_3_outof_6) {
+                        roommate_print.value_weights[t][0] = 2;
+                        roommate_print.value_weights[t][3] = 2;
+                    } else if (rand_num_ > threshold_4_outof_6) {
+                        roommate_print.value_weights[t][1] = 2;
+                        roommate_print.value_weights[t][2] = 2;
+                    } else if (rand_num_ > threshold_5_outof_6) {
+                        roommate_print.value_weights[t][1] = 2;
+                        roommate_print.value_weights[t][3] = 2;
+                    } else {
+                        roommate_print.value_weights[t][2] = 2;
+                        roommate_print.value_weights[t][3] = 2;
+                    }
+                }
+
+                // Set the value_action array
+                for (int v=0; v<4; v++){
+                    // Add 0.5 to a randomly selected activity for four times.
+                    // In this way, each activity's expected utility is 0.5.
+                    for (int tt = 0; tt < 4; tt++){
+                        double rand_select = random.nextDouble();
+                        if (rand_select > threshold_1_outof_4){
+                            roommate_print.action_value[t][v][0] += 0.5;
+                        } else if (rand_select > threshold_2_outof_4) {
+                            roommate_print.action_value[t][v][1] += 0.5;
+                        } else if (rand_select > threshold_3_outof_4){
+                            roommate_print.action_value[t][v][2] += 0.5;
+                        } else {
+                            roommate_print.action_value[t][v][3] += 0.5;
+                        }
+                    }
+                }
+            }
+
+            // Initialize the utility array:
+            for (int n=0; n<2000; n++){
+                roommate_print.utility[n] = 0;
+                roommate_print.utility_primary[n] = 0;
+                roommate_print.utility_conservative[n] = 0;
+            }
+        }
+
+        //Each agent keeps lists of their roommates
+        Roommate_print temp;
+        int i = 0;
+        for(i=0; i<numRoommates; i++){
+            Roommate_print roommate_print = (Roommate_print) allRoommates.get(i);
+            for(int j=0; j< allRoommates.size();j++){
+                if (i==j) continue;
+                temp = (Roommate_print) allRoommates.get(j);
+
+                //keep references to members in my circles.
+                if (roommate_print.dormNum==temp.dormNum)
+                    roommate_print.myDorm.add(temp);
+            }
+            schedule.scheduleRepeating(roommate_print, 0, 1.0);
+        }
+    }
+
+    public void finish(){
+        super.finish();
+
+        // Extract the utility for all agents:
+        for(int i=0; i<numRoommates; i++){
+            Roommate_print roommate_print = (Roommate_print) allRoommates.get(i);
+            System.arraycopy(roommate_print.utility, 0, utility_record[i], 0, 2000);
+            System.arraycopy(roommate_print.utility_primary, 0, utility_record_primary[i], 0, 2000);
+            System.arraycopy(roommate_print.utility_conservative, 0, utility_record_conservative[i], 0, 2000);
+        }
+
+        // A test output below:
+        Roommate_print roommate_print0 = (Roommate_print) allRoommates.get(0);
+        Roommate_print roommate_print1 = (Roommate_print) allRoommates.get(1);
+        System.out.println(roommate_print0.dormNum);
+        System.out.println(roommate_print1.dormNum);
+        System.out.println(roommate_print0.utility[0] + roommate_print1.utility[0]);
+        System.out.println(roommate_print0.utility[1] + roommate_print1.utility[1]);
+        System.out.println(roommate_print0.utility[2] + roommate_print1.utility[2]);
+        System.out.println(roommate_print0.utility[3] + roommate_print1.utility[3]);
+        System.out.println(roommate_print0.utility[4] + roommate_print1.utility[4]);
+        System.out.println(roommate_print0.utility[5] + roommate_print1.utility[5]);
+        System.out.println(roommate_print0.utility[6] + roommate_print1.utility[6]);
+        System.out.println(roommate_print0.utility[7] + roommate_print1.utility[7]);
+        System.out.println(roommate_print0.utility[8] + roommate_print1.utility[8]);
+        System.out.println(roommate_print0.utility[9] + roommate_print1.utility[9]);
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("utility_record.txt", "UTF-8");
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        writer.println(Arrays.deepToString(utility_record));
+        writer.close();
+
+        try {
+            writer = new PrintWriter("utility_record_primary.txt", "UTF-8");
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        writer.println(Arrays.deepToString(utility_record_primary));
+        writer.close();
+
+        try {
+            writer = new PrintWriter("utility_record_conservative.txt", "UTF-8");
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        writer.println(Arrays.deepToString(utility_record_conservative));
+        writer.close();
+
+        try {
+            writer = new PrintWriter("utility_distribution.txt", "UTF-8");
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        writer.println(Arrays.toString(utility_distri));
+        writer.close();
+    }
+
+    public static void main(String[] args)
+    {
+        doLoop(Roommates_print.class, args);
+        System.exit(0);
+    }
+}
